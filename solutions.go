9fafe2910ff9881887e51c5e4331ba969ef376a6
jackyx-zhou <33125377+jackyx-zhou@users.noreply.github.com>
2019-10-23 11:54:16 +0100

    Update solutions.go

--- solutions.go
+++ solutions.go
@@ -81,4 +81,68 @@ func main() {
 }
 
 // ---------------------------------------------------------
+// Exercise: Equivalent Binary Trees
+package main
 
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t != nil {
+		Walk(t.Left, ch)
+		ch <- t.Value
+		Walk(t.Right, ch)
+	}
+}
+
+// Since we use range with channel, 
+// it needs to be closed to finish the blocking and exit loop
+func WalkTree (t *tree.Tree, ch chan int) {
+	Walk(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+// Since it's a sorted tree we can determine equivalence by inspecting each element
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	var arr1, arr2 []int
+	go WalkTree(t1, ch1)
+	
+	ch2 := make(chan int)
+	go WalkTree(t2, ch2)
+	
+	for i1 := range ch1 {
+		arr1 = append(arr1, i1)
+	}
+	
+	for i2 := range ch2 {
+		arr2 = append(arr2, i2)
+	}
+	
+	if len(arr1) != len(arr2) {
+        return false
+    }
+	// sorted tree
+    for i, v := range arr1 {
+        if v != arr2[i] {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+	ch1 := make(chan int)
+	go WalkTree(tree.New(2), ch1)
+	for i := range ch1 {
+		fmt.Println(i)
+	}
+	fmt.Println("---------------------------")
+	fmt.Println("Should return true:", Same(tree.New(1), tree.New(1)))
+	fmt.Println("Should return false:", Same(tree.New(1), tree.New(2)))
+}
